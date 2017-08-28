78abbd31b64bbe020bc6e75088db1d7fca656696
Alex Ankudinov <xelez0@gmail.com>
2017-08-28 19:25:58 +0500

    Replicate magic_quotes_gpc

--- globals.php
+++ globals.php
@@ -15,6 +15,16 @@
   $IFACE = 'SPAWNING NEW IFACE';
   $DOCUMENT_ROOT = __DIR__;
 
+
+  /* Replicate magic_quotes_gpc = True */
+  foreach($_POST as $key => $val) {
+        $_POST[$key] = addslashes($val);
+  }
+  foreach($_GET as $key => $val) {
+        $_GET[$key] = addslashes($val);
+  }
+
+  /* Replicate register_globals = True */
   $superglobals = array($_FILES, $_POST, $_GET);
   foreach ($superglobals as $superglobal) {
     extract($superglobal, EXTR_SKIP);
